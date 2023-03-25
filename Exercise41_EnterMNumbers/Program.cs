c0642dbf8c9ebad4236c8e63474bfe557619d061
Tatyana_Reshetova <vlasenko2205@gmail.com>
2023-03-25 14:34:08 +0300

    количество чисел больше 0

---
+++ Exercise41_EnterMNumbers/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+//сколько чисел больше 0 ввёл пользователь.
+
+//0, 7, 8, -2, -2 -> 2
+
+//1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write("Введите числа: ");
+string[] array = Console.ReadLine().Split(" ");
+Console.Write($"[{string.Join (", ", array)}] -> {CountPos(array)}");
+
+int CountPos(string[] array){
+    int count = 0;
+        foreach (string el in array){
+            count += int.Parse(el) > 0 ? 1 : 0;}
+    return count;}
+
+
